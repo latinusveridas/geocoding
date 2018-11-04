649311187b236093b0a86d61e5a783384a7d6a7f
John D <sparteclaudius749@gmail.com>
2018-11-04 11:11:42 +0100

    added query

--- app.js
+++ app.js
@@ -22,36 +22,63 @@ app.get('/',function(req,res) {
         if (err) {
             console.log(err)
         } else {
+
             console.log('Success on connection to the database')
+                        //===========DEBUG PARSE USER REQUEST
+            var userRequest = '21 lotissement les peupliers marvejols';
+            var splitted = userRequest.split(" ");
+            var resNumberHome = splitted[0];
+            //===========DEBUG PARSE USER REQUEST
+
+            geocoder.geocode(userRequest, function(err,res) {
+
+                //SHOW RESPONSE
+                console.log(res);
+
+                // RETURNED RESPONSE IS AN ARRAY WITH ONE JSON OBJECT
+                var resArray = res[0];
+
+                // SELECT OUR KEYS
+                var resStreet = resArray.streetName;
+                var resCity = resArray.city;
+                var resState = resArray.state;
+                console.log(resNumberHome,resStreet, resCity,resState);
+
+                // SELECT LATITUDE & LONGITUDE
+                var resLatitude = resArray.latitude;
+                var resLongitude = resArray.longitude;
+
+
+                //INSERT IN DATABASE
+                var targetTable = "sampledb.events_location";
+                var locationData = {
+                    "event_id": "pppppp",
+                    "latitude": resLatitude,
+                    "longitude": resLongitude,
+                    "country": resState,
+                    "city": resCity,
+                    "street_name": resStreet,
+                    "street_number": resNumberHome 
+                }; 
+
+                con.query('INSERT INTO ? SET ??',[targetTable,locationData],function(err,result,fields){
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        console.log(result)
+                        console.log("Success in insert")
+                    }
+
+                });
+
+
+            });
+
         }
 
     });
 
-    //===========DEBUG PARSE USER REQUEST
-    var userRequest = '21 lotissement les peupliers marvejols';
-    var splitted = userRequest.split(" ");
-    var resNumberHome = splitted[0];
-    //===========DEBUG PARSE USER REQUEST
 
-    geocoder.geocode(userRequest, function(err,res) {
-
-        //SHOW RESPONSE
-        console.log(res);
-
-        // RETURNED RESPONSE IS AN ARRAY WITH ONE JSON OBJECT
-        var resArray = res[0];
-
-        // SELECT OUR KEYS
-        var resStreet = resArray.streetName
-        var resCity = resArray.city
-        var resState = resArray.state
-        console.log(resNumberHome,resStreet, resCity,resState);
-
-        // SELECT LATITUDE & LONGITUDE
-        var resLatitue = resArray.latitude;
-        var resLongitude = resArray.longitude;
-
-    });
 
 
 });
