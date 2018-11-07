52520d9678df910c63434078aaa7c687fed59012
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2018-11-07 15:03:38 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -22,13 +22,13 @@ app.get('/',function(req,res) {
         //ON DEFINIE targetTable & locationData
         var resTable = JSON.stringify(callback[0])
         var locationData = [
-            callback[1][0],
-            callback[1][1],
-            callback[1][2],
-            callback[1][3],
-            callback[1][4],
-            callback[1][5],
-            callback[1][6],
+            "'"+callback[1][0]+"'",
+            "'" + callback[1][1] + "'",
+            "'" + callback[1][2] + "'",
+            "'" + callback[1][3] + "'",
+            "'" + callback[1][4] + "'",
+            "'" + callback[1][5] + "'",
+            "'" + callback[1][6] + "'",
         ]
 
         console.log(resTable)
@@ -43,6 +43,12 @@ app.get('/',function(req,res) {
     
 
 });
+
+
+app.get('/as',Geo.geocodeFunction('21 lotissement les peupliers 48100 Marvejols' {
+
+
+};
 
 
 //=====SERVER LAUNCH========
