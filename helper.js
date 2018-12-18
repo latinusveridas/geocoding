977193ed422ce81d14f2dc84e51917fa94cca6ae
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2018-12-18 14:57:22 +0100

    Create helper.js

---
+++ helper.js
@@ -0,0 +1,20 @@
+
+
+var possibleSports = [
+yoga,
+running,
+tracking,
+canoe,
+sailing,
+bowling,
+snooker,
+zumba,
+crossfit,
+
+]
+
+func nbSportsAvailable() {
+
+var nb = possibleSports.length
+console.log("There is ",nb," available sports in cache system")
+}
