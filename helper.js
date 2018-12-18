81276645a69522c3f1bec92170e8d6ff269bd509
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2018-12-18 14:58:24 +0100

    Update helper.js

--- helper.js
+++ helper.js
@@ -1,4 +1,10 @@
 
+//EXPRESS
+var express = require('express');
+var app = express();
+
+var bodyParser = require("body-parser");
+
 
 var possibleSports = [
 yoga,
@@ -10,11 +16,11 @@ bowling,
 snooker,
 zumba,
 crossfit,
-
 ]
 
 func nbSportsAvailable() {
-
+  
 var nb = possibleSports.length
 console.log("There is ",nb," available sports in cache system")
+  
 }
