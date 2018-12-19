69b7451744d8b69d2848e943fd2ff285620adc01
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2018-12-19 16:54:39 +0100

    Update events.js

--- Events/events.js
+++ Events/events.js
@@ -104,9 +104,9 @@ router.post('/createevent', function (req,res) {
         ]
 
         //On sauve la data location dans la table events_location
-        Pool.insertinto(resTable, locationData, function(callback) {
+        Pool.insertinto(resTable, locationData, function(rows) {
 
-            if callback.affectedRows = 1 {
+            if rows.affectedRows = 1 {
             isSuccessTableLocation = true
             } else {
             isSuccessTableLocation = false
