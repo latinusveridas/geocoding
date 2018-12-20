050437b4d60818ad5246926536631ff0e83052b7
John <sparteclaudius749@gmail.com>
2018-12-20 21:25:32 +0100

    fixed helper

--- helper.js
+++ helper.js
@@ -9,26 +9,30 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 var possibleSports = [
-yoga,
-running,
-tracking,
-canoe,
-sailing,
-bowling,
-snooker,
-zumba,
-crossfit,
+"yoga",
+"running",
+"tracking",
+"canoe",
+"sailing",
+"bowling",
+"snooker",
+"zumba",
+"crossfit",
 ]
 
-func nbSportsAvailable() {
+app.get('/sports',function(req, res) {
+res.status(204).json(possibleSports)
+});
+
+/*func nbSportsAvailable() {
   
 var nb = possibleSports.length
 console.log("There is ",nb," available sports in cache system")
   
-}
+}*/
 
 //=====SERVER LAUNCH========
 // Helper on port 3001
-app.listen(3001,function(req,res){
+app.listen(3000,function(req,res){
     console.log('HELPER ON GOING');
 });
