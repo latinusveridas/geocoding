5ea253a3cd72618937b3272980c35e7ebfc669c5
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2019-01-02 17:50:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,19 +3,25 @@
 var express = require('express');
 var app = express();
 
+//BODYPARSER
 var bodyParser = require("body-parser");
 
+// EVENTS
 var events = require('./Events/events');
 app.use('/events', events);
 
+//USERS
 var users = require('./Users/users');
 app.use('/users', users);
 
+//AUTHENTIFICATION
 var authentification = require('./Authentification/authentification');
 app.use('/auth', authentification);
 
+//PUBLIC PICTURES
 app.use(express.static('Public'));
 
+//BODYPARSER
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
