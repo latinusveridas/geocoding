59c5303889ea5267af54ed5da7f4ed7d1978fb37
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2019-01-02 17:58:07 +0100

    Update ratingsHelper.js

--- ratingsHelper.js
+++ ratingsHelper.js
@@ -1,3 +1,13 @@
+
+//EXPRESS
+var express = require('express');
+var app = express();
+
+//BODYPARSER
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // VARIABLES
 var mysql = require('mysql');
 
