33f755969c2caff73f19a116dcf5604a73d46b4f
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2019-01-03 13:20:51 +0100

    Update WorksDates.js

--- WorksDates.js
+++ WorksDates.js
@@ -1,2 +1,37 @@
+//EXPRESS
+var express = require('express');
+var app = express();
 
-var express = Express();
+//BODYPARSER
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// VARIABLES
+var mysql = require('mysql');
+
+// DEFINE POOLING
+var pool_events = mysql.createPool({
+    connectionLimit: 10,
+    host: "83.217.132.102",
+    port: '3306',
+    user: "root",
+    password: "Miroslava326356$$$$$",
+    database: "events"
+});
+
+module.exports.pool_events = pool_events;
+
+// =====================================================
+
+app.get('/dates', function(req, res) {
+    console.log("in ratings !")
+    
+var resMain = {
+    "error": 0,
+    "error_description": "",
+    "success" : "",
+    "type_data" : "",
+    "data" : {}
+}
+    
