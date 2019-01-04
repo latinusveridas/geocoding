368c2eeeddeda92ebbd0c4a41f519dc07da09323
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2019-01-04 15:36:28 +0100

    Update WorksDates.js

--- WorksDates.js
+++ WorksDates.js
@@ -71,13 +71,10 @@ app.get('/dates', function(req, res) {
             var tabl_year = elem.substring(0,4);
             console.log("item ", elem, tabl_year)
             
-            var tabl_week = elem.substring(7,8)
+            var tabl_week = elem.substring(7,8);
             console.log("item ", elem, tabl_week)
             
         }
-            
-}
-      
         
     });
     
