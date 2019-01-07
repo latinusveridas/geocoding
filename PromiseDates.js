b3a9893b044aac38725002548bee8b8244f205d2
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2019-01-07 16:50:08 +0100

    Update PromiseDates.js

--- PromiseDates.js
+++ PromiseDates.js
@@ -272,9 +272,9 @@ function tableShouldBeTested(i) {
 
 		// Split text du table pour obtenir Year et Week
 		var tabl_year = i.substring(0,4);
-		console.log("item ", elem, tabl_year)
+		console.log("item ", i, tabl_year)
 		var tabl_week = i.substring(6,8);
-		console.log("item ", elem, tabl_week)
+		console.log("item ", i, tabl_week)
 
 		// On loop dans les annees
 		if (parseInt(tabl_year) < curr_year) {
