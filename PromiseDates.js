2475fb177bfeaa0673eaffd1a2a03622b6325ef1
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2019-01-07 10:23:36 +0100

    Update PromiseDates.js

--- PromiseDates.js
+++ PromiseDates.js
@@ -39,7 +39,7 @@ var resMain = {
 
 app.get('/dates', function(req, res) {
    
-async function collAllEvents() {
+function collAllEvents() {
 
     console.log("log : Launch of Date debug for databases")
 
@@ -62,7 +62,7 @@ async function collAllEvents() {
     var coll_tables = 'SHOW TABLES'
     var collection_tables = []
     
-    await basicquery(coll_tables, function (callback) {
+    basicquery(coll_tables, function (callback) {
     collection_tables = callback.map(v => v.Tables_in_events);
     console.log(collection_tables);
         
@@ -91,7 +91,7 @@ async function collAllEvents() {
                 // organizer_id = "debug_organizer_id_1"
                 var selec_query = "SELECT * FROM " + elem + " WHERE organizer_id = " + organizer_id
                 
-                await basicquery(selec_query, function (callback) {
+                basicquery(selec_query, function (callback) {
                 collected_events.push(callback)
                 console.log(callback)
                 }); 
@@ -109,7 +109,7 @@ async function collAllEvents() {
 			    
                     var selec_query = "SELECT * FROM " + elem + " WHERE organizer_id = " + organizer_id
                     
-                    await basicquery(selec_query, function (callback) {
+                    basicquery(selec_query, function (callback) {
                     collected_events.push(callback)
                     console.log(callback)
                     }); 
@@ -120,7 +120,7 @@ async function collAllEvents() {
                             // = We are in present events tables
                             console.log("We start to check the table because its in the same year and same week")
 
-                        await basicquery(selec_query, function (callback) {
+                        basicquery(selec_query, function (callback) {
                         collected_events.push(callback)
                         console.log(callback)
                         }); 
