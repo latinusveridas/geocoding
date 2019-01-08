c9eb13ae7161a4503e83345cb05dff4d69ebf991
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2019-01-08 14:38:24 +0100

    Update PromiseDates.js

--- PromiseDates.js
+++ PromiseDates.js
@@ -106,6 +106,27 @@ app.get('/dates2', function (req,res) {
 	}) // Appel de la fonction
 })
 
+/*
+function delay() {
+  return new Promise(resolve => setTimeout(resolve, 300));
+}
+
+async function delayedLog(item) {
+  // notice that we can await a function
+  // that returns a promise
+  await delay();
+  console.log(item);
+}
+async function processArray(array) {
+  array.forEach(async (item) => {
+    await func(item);
+  })
+  console.log('Done!');
+}
+
+processArray([1, 2, 3]);
+*/
+
 // ================ SERVER LAUNCH ======================
 // Helper on port 3001
 app.listen(3002,function(req,res){
