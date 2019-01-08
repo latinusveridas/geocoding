4ac471ac6709863a5e74fb3ee67646779032a8a7
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2019-01-08 09:03:11 +0100

    Update PromiseDates.js

--- PromiseDates.js
+++ PromiseDates.js
@@ -64,11 +64,12 @@ app.get('/dates2', function (req,res) {
 					console.log("collenction lenght", collection_tables.length)
 					
 					if ( i == collection_tables.length - 1 ) {
-						console.log("IN FINAL !!")
-						console.log("i = ", i)
-						res.json(CollectedOrganizedEvents)
+						//console.log("IN FINAL !!")
+						//console.log("i = ", i)
+						//res.json(CollectedOrganizedEvents)
+						console.log("EQUAL")
 					} else {
-						
+						console.log("BELOW")
 					}
 				})
 				
