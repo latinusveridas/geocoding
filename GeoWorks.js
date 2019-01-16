2b8ebc2b26d377b70951ee79b3d9e6fb5821ca44
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2019-01-16 16:16:29 +0100

    Update GeoWorks.js

--- GeoWorks.js
+++ GeoWorks.js
@@ -12,8 +12,50 @@ var mathjs = require('mathjs');
 
 
 
+app.get('/inside', function (req,res) {
+
+
+google.maps.event.addListener(map, 'click', function(event) {
+  var click = event.latLng;
+  var locs = {lat: event.latLng.lat(), lng: event.latLng.lng()};
+  var n = arePointsNear(user, locs, diameter);
+
+     if(n){
+    marker = new google.maps.Marker({
+      map: map,
+      position: locs,
+      label: {
+        text:"I", //marking all jobs inside radius with I
+        color:"white"
+      }
+    });
+         
+           }else{
+    marker = new google.maps.Marker({
+      map: map,
+      position: locs,
+      label: {
+        text:"O", //marking all jobs outside radius with O
+        color:"white"
+      }
+    });
+  }
+});
+
+
+})
+
+
 //=====SERVER LAUNCH========
 // Helper on port 3001
 app.listen(3002,function(req,res){
     console.log('GEO WORKS ON GOING');
 });
+
+function arePointsNear(checkPoint, centerPoint, km) { 
+   var ky = 40000 / 360;
+   var kx = Math.cos(Math.PI * centerPoint.lat / 180.0) * ky;
+   var dx = Math.abs(centerPoint.lng - checkPoint.lng) * kx;
+   var dy = Math.abs(centerPoint.lat - checkPoint.lat) * ky;
+   return Math.sqrt(dx * dx + dy * dy) <= km;
+}
