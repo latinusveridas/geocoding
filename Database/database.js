22d8bd65ce2b648d935b58a81d67c7938c931b5e
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2019-01-16 11:40:08 +0100

    Update database.js

--- Database/database.js
+++ Database/database.js
@@ -2,13 +2,57 @@ var mysql = require('mysql');
 
 // ============================== NEW =====================================
 
+function CreatePool(db_loc) {
 
+    return new Promise(function (resolve,reject) {
+    
+        var pool = mysql.createPool({
+        connectionLimit: 10,
+        host: "83.217.132.102",
+        port: '3306',
+        user: "root",
+        password: "Miroslava326356$$$$$",
+        database: db_loc 
+        })    
+        
+        resolve(pool)
+    })
+}
 
+function ConnectToDB(argPool) {
+    
+    return new Promise (function (resolve,reject) {
 
+        argPool.getConnection(function (err, con) {
 
+        if (err) {
+        console.log(err)
+        } else {
+        console.log("Success to retrieve the connection")
+        resolve(con)
+        }
+            
+	})
+    })
+}
+                        
+function GoQuery(connection, query) {
 
+	return new Promise(function(resolve,reject) {
+		
+		connection.query(query, function(err,result,fields) {	
+				if (err) {
+					return reject(err)
+					console.log(err)
+				} else {
+					return resolve(result)
+				}
 
+		})
+			
+	})
 
+}
 
 
 // ============================== NEW =====================================
