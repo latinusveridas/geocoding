ad8c608a0ea594ad5beb8f030baab85ad53ad968
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2019-01-16 16:12:43 +0100

    Update GeoWorks.js

--- GeoWorks.js
+++ GeoWorks.js
@@ -8,5 +8,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // VARIABLES
-var mysql = require('mysql');
 var mathjs = require('mathjs');
+
+
+
+//=====SERVER LAUNCH========
+// Helper on port 3001
+app.listen(3002,function(req,res){
+    console.log('GEO WORKS ON GOING');
+});
