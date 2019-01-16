f4c65a4b89f64edef21f4bcc20ffec9695381201
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2019-01-16 16:12:02 +0100

    Update GeoWorks.js

--- GeoWorks.js
+++ GeoWorks.js
@@ -1,1 +1,12 @@
-//
+//EXPRESS
+var express = require('express');
+var app = express();
+
+//BODYPARSER
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// VARIABLES
+var mysql = require('mysql');
+var mathjs = require('mathjs');
