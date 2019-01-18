0d5f1c70f3576f77213c9a4eaef320d10eacafbb
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2019-01-18 11:16:15 +0100

    Update bookings.js

--- Bookings/bookings.js
+++ Bookings/bookings.js
@@ -17,3 +17,30 @@ router.use(bodyParser.urlencoded({ extended: false }));
 
 // COMPONENTS
 var DB = require('../Database/database');
+
+// ================================================================================================================================
+
+router.get('/listbooking', function(req,res) {
+
+	//Debug
+  var location = "fr"
+    
+	DB.CreatePool(location).then(currPool => {
+	DB.ConnectToDB(currPool).then(currCon => {
+		
+		var sql = `SELECT * FROM 01_bookings_${location} WHERE event_id = ?`
+		
+		DB.GoQuery(currCon,sql).then(resultPost => {
+		
+		//var packetStr = JSON.stringify(resultPost)
+		//var packetStr = JSON.parse(packetStr)
+		//console.log(resultPost)
+		res.status(200).send(resultPost)
+
+		}) //GoQuery Select
+	currCon.release()
+	}) // GetConnection
+
+	}) // CreatePool
+
+}) // Appget
