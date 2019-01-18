a231fae17d12e9dcac7ae7180360381c2bebdf74
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2019-01-18 09:32:31 +0100

    Update events.js

--- Events/events.js
+++ Events/events.js
@@ -221,61 +221,43 @@ module.exports = router;
 // GEOCODING
 function geocodeFunction(userRequest,callback) {
 
+	return new Promise(function(resolve,reject) {
+	
+	//on recupere juste le numero de maison car pb en geocoding
     var splitted = userRequest.split(" ");
     var resNumberHome = splitted[0];
 
-    var result1;
+		geocoder.geocode(userRequest, function(err,res) {
 
-    geocoder.geocode(userRequest, function(err,res) {
+			// RETURNED RESPONSE IS AN ARRAY WITH ONE JSON OBJECT
+			var resArray = res[0];
 
-        //SHOW RESPONSE
-        //console.log(res);
-    
-        // RETURNED RESPONSE IS AN ARRAY WITH ONE JSON OBJECT
-        var resArray = res[0];
+			//console.log(resArray)
+		
+			// SELECT OUR KEYS
+			var resStreet = resArray.streetName;
+			var resCity = resArray.city;
+			var resState = resArray.state;
+			//console.log(resNumberHome,resStreet, resCity,resState);
+		
+			// SELECT LATITUDE & LONGITUDE
+			var resLatitude = resArray.latitude;
+			var resLongitude = resArray.longitude;
 
-        console.log(resArray)
-    
-        // SELECT OUR KEYS
-        var resStreet = resArray.streetName;
-        var resCity = resArray.city;
-        var resState = resArray.state;
-        //console.log(resNumberHome,resStreet, resCity,resState);
-    
-        // SELECT LATITUDE & LONGITUDE
-        var resLatitude = resArray.latitude;
-        var resLongitude = resArray.longitude;
+			var locationData = {
+				"latitude": resLatitude.toString(),
+				"longitude": resLongitude.toString() ,
+				"state": resState ,
+				"city": resCity,
+				"street": resStreet,
+				"number": resNumberHome.toString()
+			}
 
-        //GENERATE EVENT ID
-        //var dt = datetime.create();
-        //var dt = dt.format('Y_m')
-        //var eventID = dt + '_E_' + rdmString.generate(40) ;
-        //console.log("GENERATED EVENT_ID " + eventID) 
-        var eventID = ""
-    
-        //PUT ALL THE STUFF IN A ARRAY
-        var targetTable = 'sampledb.events_location';
-
-        var locationData = [
-            eventID,
-            resLatitude.toString(),
-            resLongitude.toString() ,
-            resState ,
-            resCity,
-            resStreet,
-            resNumberHome.toString()
-        ];
-
-        var result1 = [
-            targetTable,
-            locationData
-        ]
-
-        console.log("INSIDE EVENTS :", result1)
-
-        return callback(result1);
-        
-    });
+			resolve(locationData);
+			
+		});
+	
+	}) // Promise
 
 };
 
