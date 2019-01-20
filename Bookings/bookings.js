af6718fb4c15f0cd032edae547b59d53a075feba
John <sparteclaudius749@gmail.com>
2019-01-20 15:38:49 +0100

    Update bookings.js

--- Bookings/bookings.js
+++ Bookings/bookings.js
@@ -80,11 +80,11 @@ router.post('/book', function (req,res) { // <-- TO BE MOVED TO POST
 
 		DB.GoQuery(currCon,sql).then(rawRes => {
 			console.log(rawRes)
-			var rawRes = JSON.stringify(rawRes)
-			//var rawRes = JSON.parse(rawRes)
+			rawRes = JSON.stringify(rawRes)
+			rawRes = JSON.parse(rawRes)
 			console.log(rawRes)
-			console.log("nb_part_sub", rawRes[0]["nb_part_sub"])
-			console.log("nb_part_max",rawRes[0]["nb_part_max"])
+			console.log("nb_part_sub", rawRes.nb_part_sub)
+			console.log("nb_part_max",rawRes["nb_part_max"])
 
 			if (rawRes["nb_part_sub"] == rawRes["nb_part_max"]) {
 				resMain.error = 1
