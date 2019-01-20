2add5f3062de71570efb268294244f6bbca8d9e1
John <sparteclaudius749@gmail.com>
2019-01-20 11:15:46 +0100

    Update bookings.js

--- Bookings/bookings.js
+++ Bookings/bookings.js
@@ -79,6 +79,8 @@ router.post('/book', function (req,res) { // <-- TO BE MOVED TO POST
 		var sql = mysql.format(bas,inserts)
 
 		DB.GoQuery(currCon,sql).then(rawRes => {
+			var rawRes = JSON.stringify(rawRes)
+			var rawRes = JSON.parse(rawRes)
 			console.log("nb_part_sub", rawRes.nb_part_sub)
 			console.log("nb_part_max",rawRes.nb_part_max)
 
