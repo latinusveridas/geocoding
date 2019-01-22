f20b5ff0c4ba58a3672bf82ba36c563df9c63a48
latinusveridas <38329691+latinusveridas@users.noreply.github.com>
2019-01-22 09:33:44 +0100

    Update newEventModel.js

--- newEventModel.js
+++ newEventModel.js
@@ -33,6 +33,13 @@ var mathjs = require('mathjs');
 const multer = require('multer');
 const path = require('path');
 
+// Set The Storage Engine
+const storage = multer.diskStorage({
+  destination: './public/uploads/',
+  filename: function(req, file, cb){
+    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
 
 // UPLOADS 
 
